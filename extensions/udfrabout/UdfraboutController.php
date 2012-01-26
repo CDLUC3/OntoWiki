e99d3d81bd8d8e96fe01af78a389752000ba3fa8
Abhishek Salve <abhishek.salve@ucop.edu>
2012-01-26 10:10:44 -0800

    UDFR documentation #2

--- extensions/udfrabout/UdfraboutController.php
+++ extensions/udfrabout/UdfraboutController.php
@@ -1,4 +1,14 @@
 <?php
+
+/**
+ * Copyright © 2012 The Regents of the University of California
+ *
+ * The Unified Digital Format Registry (UDFR) is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class UdfraboutController extends OntoWiki_Controller_Base
 {
 	/**
@@ -11,6 +21,9 @@ class UdfraboutController extends OntoWiki_Controller_Base
 		OntoWiki_Navigation::disableNavigation();
 	}
 	
+	/**
+     * Displays the UDFR Privacy policy
+     */
 	public function privacypolicyAction()
     {
 		$this->view->url = $this->_config->staticUrlBase;
